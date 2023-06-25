2e005d429c457977154a18ec42d85a0e79ccc6a8
Mikołaj Jędrzejewski <mikolajedjed@gmail.com>
2023-06-25 19:01:22 +0200

    Update for new tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,8 +3,10 @@ unzip tests_a.zip ;
 unzip tests_a_dense.zip ; 
 unzip tests_tr0.zip ; 
 unzip tests_t.zip ;
+unzip tests_t_MORE.zip ;
 cd ..; 
-echo "RUN naive.py for tests_a\n"; python3 test.py naive.py tests/tests_a;
-echo "RUN naive.py for tests_a_dense\n"; python3 test.py naive.py tests/tests_a_dense;
-echo "run trees.py for tests_tr0\n"; python3 test.py trees.py tests/tests_tr0;
-echo "run trees.py for tests_t\n"; python3 test.py trees.py tests/tests_t;
+echo "RUN naive.py for tests_a"; python3 test.py naive.py tests/tests_a;
+echo "RUN naive.py for tests_a_dense"; python3 test.py naive.py tests/tests_a_dense;
+echo "RUN trees.py for tests_tr0"; python3 test.py trees.py tests/tests_tr0;
+echo "RUN trees.py for tests_t"; python3 test.py trees.py tests/tests_t;
+echo "RUN trees.py for tests_t_MORE"; python3 test.py trees.py tests/tests_t_MORE
